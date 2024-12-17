56fa0b2a029a7688f17973997e9bb8a22d0ca7f5
brandon <brandonlaursen95@yahoo.com>
2024-12-17 17:19:23 -0500

    added user table migration and seeds

---
+++ backend/db/migrations/20241217215121-create-user.js
@@ -0,0 +1,68 @@
+"use strict";
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
+
+console.log(options)
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "Users",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        username: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        hashedPassword: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          unique: true,
+        },
+        balance: {
+          type: Sequelize.INTEGER,
+          defaultValue: 0,
+        },
+        firstName: {
+          type: Sequelize.STRING,
+        },
+        lastName: {
+          type: Sequelize.STRING,
+        },
+
+        profilePic: {
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+      },
+      options
+    );
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    return queryInterface.dropTable(options);
+  },
+};
