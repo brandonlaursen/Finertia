c28c0ed907ea879143ccea40367e563012cb1941
brandon <brandonlaursen95@yahoo.com>
2024-12-19 13:33:10 -0500

    added user auth express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,50 @@
 const router = require('express').Router();
+const { User } = require('../../db/models');
+const { restoreUser, setTokenCookie, requireAuth  } = require('../../utils/auth.js');
+
+// * keep before other middlewear
+router.use(restoreUser);
 
 
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-});
+
+
+
+
+
+// * for testing
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'Demo-lition'
+//     }
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user: user });
+// });
+
+
+
+// router.get(
+//   '/restore-user',
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+
+
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+
+// router.post('/test', function(req, res) {
+//   res.json({ requestBody: req.body });
+// });
 
 
 
