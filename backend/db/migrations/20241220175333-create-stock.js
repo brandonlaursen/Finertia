19f80921cda0d6d3816ab63232ef9b7d78529369
brandon <brandonlaursen95@yahoo.com>
2024-12-20 13:03:48 -0500

    added stock migrations, models, and seeders

---
+++ backend/db/migrations/20241220175333-create-stock.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Stocks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      stockName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      stockSymbol: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      currentPrice: {
+        type: Sequelize.INTEGER
+      },
+      marketCap: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      }
+    },
+  options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Stocks";
+    await queryInterface.dropTable('Stocks');
+  }
+};
---
+++ backend/db/models/stock.js
@@ -0,0 +1,35 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Stock extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Stock.init(
+    {
+      stockName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      stockSymbol: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      currentPrice: DataTypes.INTEGER,
+      marketCap: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Stock",
+    }
+  );
+  return Stock;
+};
