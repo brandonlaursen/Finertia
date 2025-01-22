281bf881902b26262e79f96986446482c38e6317
brandon <brandonlaursen95@yahoo.com>
2025-01-22 13:08:20 -0500

    log in page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,44 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import { restoreUser } from "../store/session";
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      dispatch(restoreUser());
+      setIsLoaded(true);
+    };
+
+    loadUser();
+
+  }, [dispatch]);
+
+  return <>{isLoaded && <Outlet />}</>;
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <h1>Welcome!</h1>,
+      },
+      {
+        path: "/login",
+        element: <LoginFormPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <h1> Hello from App </h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// import "./index.css";
+import "./index.css";
 import { Provider } from "react-redux";
 import configureStore from "../store";
 import { restoreCSRF, csrfFetch } from "../store/csrf";
@@ -14,7 +14,6 @@ if (import.meta.env.MODE !== "production") {
   window.csrfFetch = csrfFetch;
   window.store = store;
 }
-console.log(import.meta.env.MODE);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
