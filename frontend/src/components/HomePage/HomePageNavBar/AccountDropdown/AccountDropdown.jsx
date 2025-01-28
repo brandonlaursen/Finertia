5d512ac2583a3deca0dc86b47e258fb64b78656f
brandon <brandonlaursen95@yahoo.com>
2025-01-28 16:39:26 -0500

    updated styling for account dropdown

--- frontend/src/components/HomePage/HomePageNavBar/AccountDropdown/AccountDropdown.jsx
+++ frontend/src/components/HomePage/HomePageNavBar/AccountDropdown/AccountDropdown.jsx
@@ -44,7 +44,7 @@ function AccountDropdown({ sessionUser }) {
 
   return (
     <span tabIndex={0} ref={accountDropdownRef} onClick={toggleAccountDropdown}>
-      <Link>Account</Link>
+      <span>Account</span>
       {isAccountOpen && (
         <div className="account-dropdown-container">
           <div className="account-dropdown-user">
