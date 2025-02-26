3c1269f516269b1ba74e23d4395b3cd949a91840
brandon <brandonlaursen95@yahoo.com>
2025-02-26 17:57:46 -0500

    display percent change on home page;

--- frontend/src/components/Home/HomePage/HomePage.jsx
+++ frontend/src/components/Home/HomePage/HomePage.jsx
@@ -1,4 +1,6 @@
 import "./HomePage.css";
+import { GoTriangleDown } from "react-icons/go";
+import { GoTriangleUp } from "react-icons/go";
 
 import { selectUser } from "../../../../store/session";
 
@@ -21,7 +23,14 @@ function HomePage() {
 
   const [selectedTimeFrame, setSelectedTimeFrame] = useState("1D");
 
+  const { oneDayFiveMinAggregates } = stockSummary;
 
+  const first = oneDayFiveMinAggregates[0].y;
+  const last = oneDayFiveMinAggregates[oneDayFiveMinAggregates.length - 1].y;
+  console.log(first, last);
+  const percentChange = (((last - first) / first) * 100).toFixed(2);
+  const amountChange = (last - first).toFixed(2);
+  console.log(amountChange);
 
   return (
     <div className="HomePage">
@@ -31,14 +40,49 @@ function HomePage() {
             <div className="HomePage__main__title">Investing</div>
             <div className="HomePage__main__banner">
               <div className="HomePage__main__user-info">
-                <span>${stockSummary.totalInvestments}</span>
+                <span className={`HomePage__investments`}>
+                  ${stockSummary.totalInvestments}
+                </span>
+                <span className={`HomePage__percent-change`}>
+                  {percentChange > 0 ? (
+                    <GoTriangleUp className="HomePage__triangleIconUp" />
+                  ) : (
+                    <GoTriangleDown className="HomePage__triangleIconDown" />
+                  )}
+                  <span
+                    className={`${
+                      amountChange < 0
+                        ? "negative"
+                        : amountChange > 0
+                        ? "positive"
+                        : ""
+                    }`}
+                  >
+                    ${amountChange}
+                  </span>
+                  <span
+                    className={`${
+                      percentChange < 0
+                        ? "negative"
+                        : percentChange > 0
+                        ? "positive"
+                        : ""
+                    }`}
+                  >
+                    {`(${percentChange}%) `}
+                  </span>
+                  <span> Today</span>
+                </span>
               </div>
               <div className="HomePage__main__chart-container">
-                <HomePageChart stockSummary={stockSummary} selectedTimeFrame={selectedTimeFrame}/>
+                <HomePageChart
+                  stockSummary={stockSummary}
+                  selectedTimeFrame={selectedTimeFrame}
+                />
                 <SelectTimeFrame
-                selectedTimeFrame={selectedTimeFrame}
-                setSelectedTimeFrame={setSelectedTimeFrame}
-              />
+                  selectedTimeFrame={selectedTimeFrame}
+                  setSelectedTimeFrame={setSelectedTimeFrame}
+                />
               </div>
               <BuyingPowerDropDown sessionUser={sessionUser} />
             </div>
