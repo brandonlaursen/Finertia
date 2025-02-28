d60936280267a8dd31bc5bbff16e0da397321196
brandon <brandonlaursen95@yahoo.com>
2025-02-28 14:09:57 -0500

    stock page

--- frontend/src/components/Stock/StockNews/StockNews.jsx
+++ frontend/src/components/Stock/StockNews/StockNews.jsx
@@ -1,31 +1,59 @@
 import "./StockNews.css";
 
 function StockNews({ stockNews }) {
+  function timeAgo(timestamp) {
+    const unixTimestamp = Math.floor(new Date(timestamp).getTime() / 1000);
+    const now = Date.now() / 1000;
+    const diff = now - unixTimestamp;
+
+    const minutes = Math.floor(diff / 60);
+    const hours = Math.floor(diff / 3600);
+    const days = Math.floor(diff / (3600 * 24));
+
+    if (days > 0) {
+      return `${days}d`;
+    } else if (hours > 0) {
+      return `${hours}h`;
+    } else if (minutes > 0) {
+      return `${minutes}m`;
+    } else {
+      return `Just now`;
+    }
+  }
+
   return (
     <>
-      <span className="news-title">News</span>
-      <div className="stock-news-container">
+      <header className="StockNews__header">News</header>
+      <section className="StockNews__articles">
         {stockNews &&
           stockNews.map((news) => {
             return (
-              <a href={news.url} key={news.id}>
-                <div className="news-container">
-                  <div className="news-text">
-                    <span className="news-header">
-                      <span>{news.author}</span>
-                      <span>{news.published_utc?.split("T")[0]}</span>
-                    </span>
-                    <span className="news-main-text">{news.title}</span>
-                    <span className="news-sub-text">{news.description}</span>
+              <a href={news.article_url} key={news.id}>
+                <div className="StockNews__article">
+                  <div className="StockNews__article-main">
+                    <section className="StockNews__article-author">
+                      <span>
+                        {news.author}
+                        <span className="StockNews__article-time">
+                          {timeAgo(news.published_utc)}
+                        </span>
+                      </span>
+                    </section>
+                    <section className="StockNews__article-title">
+                      {news.title}
+                    </section>
+                    <section className="StockNews__article-contents">
+                      {news.description}
+                    </section>
                   </div>
-                  <div className="news-image">
+                  <section className="StockNews__article-image">
                     {news.image_url && <img src={news.image_url} />}
-                  </div>
+                  </section>
                 </div>
               </a>
             );
           })}
-      </div>
+      </section>
     </>
   );
 }
