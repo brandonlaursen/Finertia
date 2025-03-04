92ff4514d6422384073eda42eea2473914b0826f
brandon <brandonlaursen95@yahoo.com>
2025-03-04 18:07:02 -0500

    notification pop up

--- frontend/src/components/NotificationPopUp/NotificationPopUp.jsx
+++ frontend/src/components/NotificationPopUp/NotificationPopUp.jsx
@@ -6,10 +6,12 @@ function NotificationPopUp({ message, setNotifications }) {
     <div className="NotificationPopUp">
       <span>{message}</span>
       <span className="NotificationPopUp-view">
-        <IoClose
-          className="NotificationPopUp-icon"
-          onClick={() => setNotifications(false)}
-        />
+        <button className="NotificationPopUp-close-button">
+          <IoClose
+            className="NotificationPopUp-close-icon"
+            onClick={() => setNotifications(false)}
+          />
+        </button>
       </span>
     </div>
   );
