089710ada23a4ea8d61e78c9bab5d7a4ff5d2656
brandon <brandonlaursen95@yahoo.com>
2025-03-10 17:00:10 -0400

    refactor

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,8 @@
 // backend/routes/index.js
 const express = require("express");
 const router = express.Router();
-
 const apiRouter = require("./api");
+
 router.use("/api", apiRouter);
 
 // Serve React build files in production
@@ -28,10 +28,14 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// Add a XSRF-TOKEN cookie in development
 if (process.env.NODE_ENV !== "production") {
   router.get("/api/csrf/restore", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.json({});
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      "XSRF-Token": csrfToken,
+    });
   });
 }
 
