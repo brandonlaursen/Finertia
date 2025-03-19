c55b554a4867329c9c7522a428aee34bb559f1d1
brandon <brandonlaursen95@yahoo.com>
2025-03-19 14:07:27 -0400

    refactor

--- frontend/src/pages/WelcomePage/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage/WelcomePage.jsx
@@ -1,9 +1,4 @@
 import "./WelcomePage.css";
-// import { LuInfo } from "react-icons/lu";
-// import { FaSpaceShuttle } from "react-icons/fa";
-// import { FaGithub } from "react-icons/fa6";
-// import { FaLinkedin } from "react-icons/fa6";
-// import { FaUserAstronaut } from "react-icons/fa6";
 
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -21,7 +16,6 @@ function WelcomePage() {
 
       <section className="WelcomePage__section WelcomePage__hero">
         <div className="WelcomePage__hero-title">
-          {/* <FaSpaceShuttle /> */}
           <img src="/favicon3.png" alt="Logo" width="20" />
           <span>Finertia Investing</span>
         </div>
@@ -38,66 +32,6 @@ function WelcomePage() {
           )}
         </div>
       </section>
-
-      {/* <section className="WelcomePage__section WelcomePage__features">
-        <figure className="WelcomePage__features-image" />
-
-        <div className="WelcomePage__features-contents">
-          <header className="WelcomePage__features-contents__header">
-            Investing
-          </header>
-          <sub className="WelcomePage__features-contents__sub-header">
-            Build your portfolio starting with just $1
-          </sub>
-          <text className="WelcomePage__features-contents__text">
-            Invest in stocks, at your pace and commission-free
-          </text>
-          <small className="WelcomePage__features-contents__disclosure">
-            <LuInfo className="WelcomePage__features-contents__info-icon" />
-            Investing Disclosures
-          </small>
-          <button className="WelcomePage__features-content__button">
-            Learn More
-          </button>
-        </div>
-      </section>
-
-      <section className="WelcomePage__join WelcomePage__section ">
-        <figure className="WelcomePage__join__image" />
-        <header className="WelcomePage__join__header">
-          Join a new generation of investors.
-        </header>
-        <button className="WelcomePage__join__button">Sign up</button>
-      </section> */}
-      {/*
-      <footer className="WelcomePage__footer">
-        <div className="footer-content">
-          <p className="footer-content">
-            <FaUserAstronaut />
-            Brandon Laursen
-          </p>
-          |
-          <a
-            href="https://www.linkedin.com/in/brandon-laursen-398563218/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer-content"
-          >
-            <FaLinkedin />
-            LinkedIn
-          </a>
-          |
-          <a
-            href="https://github.com/brandonlaursen"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="footer-content"
-          >
-            <FaGithub />
-            GitHub
-          </a>
-        </div>
-      </footer> */}
     </div>
   );
 }
